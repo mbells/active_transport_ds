d38fcef68f39557b08eb78350b651d36fc68a106
Matthew Bells <github@mbells.ca>
2024-10-30 20:03:26 -0400

    More exploration

--- fetch_data/pedestrians_cyclists/download.sh
+++ fetch_data/pedestrians_cyclists/download.sh
@@ -9,3 +9,5 @@ mkdir pedestrians_cyclists
 cd pedestrians_cyclists
 
 wget https://app2.kitchener.ca/appdocs/opendata/DBAdataSets/Trails_Counters_Pedestrians_Cyclists.csv
+
+wget -O metadata1.json https://services1.arcgis.com/qAo1OsXi67t7XgmS/ArcGIS/rest/services/Active_Transportation_Eco_Counters/FeatureServer/1?f=json
